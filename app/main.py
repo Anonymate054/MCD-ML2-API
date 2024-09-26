4fd9a9f8b4b868b28d6bca6286db3a016bb280c4
Anonymate <leng.0054@gmail.com>
2024-09-25 21:38:12 -0600

    New version and model

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
 # uvicorn app.main:app --host 0.0.0.0 --reload
-
-from fastapi import FastAPI, Depends
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from app.model.model import predict_pipeline
 from app.model.model import __version__ as model_version
+from app.model.model import predict_pipeline
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from typing import Literal
 
 app = FastAPI()
 app.title = "API - ML2"
-app.version = "0.0.1"
+app.version = "0.0.2"
 
 class Data(BaseModel):
     LOAN: float = 30.77
     MORTDUE: float = 44.62
     VALUE: float = 15.519
-    REASON: float = 0.0
-    JOB: float = 5.0
+    REASON: Literal['HomeImp', 'DebtCon'] = "HomeImp"
+    JOB: Literal['Other', 'Office', 'Sales', 'Mgr', 'ProfExe', 'Self'] = "ProfExe"
     YOJ: float = 10.0
     DEROG: float = 28.0
     DELINQ: float = 34.0
@@ -30,8 +30,8 @@ class Data(BaseModel):
                 "LOAN": 30.77,
                 "MORTDUE": 44.62,
                 "VALUE": 15.519,
-                "REASON": 0.0,
-                "JOB": 5.0,
+                "REASON": "HomeImp",
+                "JOB": "ProfExe",
                 "YOJ": 10.0,
                 "DEROG": 28.0,
                 "DELINQ": 34.0,
@@ -43,11 +43,11 @@ class Data(BaseModel):
         }
 
 class PredictionOut(BaseModel):
-    BAD: int
+    credit: str
 
 @app.get("/", tags=["Settings"])
 def home():
-    return {"health_check": "OK", "model_version": "1.0"}
+    return {"health_check": "OK", "model_version": "1.1"}
 
 @app.post("/predict/", response_model=PredictionOut, tags=["ML2"])
 def predict(payload: Data):
@@ -57,4 +57,4 @@ def predict(payload: Data):
 @app.get("/predict/", response_model=PredictionOut, tags=["ML2"])
 def predict(payload: Data = Depends()):
     prediction = predict_pipeline(payload.dict())
-    return PredictionOut(BAD=prediction)
+    return PredictionOut(credit="Accepted" if prediction == 0 else "Rejected")
--- app/model/model.py
+++ app/model/model.py
@@ -1,15 +1,15 @@
 import pandas as pd
-import pickle
+from joblib import load
 import fs
 
 __version__ = "0.1.0"
 
 BASE_DIR = fs.open_fs("./app/model/")
-MODEL_PATH = f"model-{__version__}.pkl"
+MODEL_PATH = f"pipeline_model_tuning-{__version__}.joblib"
 MODEL_DIR = BASE_DIR.getsyspath(MODEL_PATH)
 
 with open(MODEL_DIR, "rb") as f:
-    model = pickle.load(f)
+    model = load(f)
 
 def predict_pipeline(json_request: dict) -> dict:
     input_data = pd.DataFrame([json_request])
